7ae279f3d32391e6d1d61744d06a45b579b161ec
Muhammed Basaran <mbasaran0293@gmail.com>
2022-10-27 20:44:16 +0200

    add js for button, somme text

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
 //Show Answer:
 
-const showButton = document.querySelectorAll('[data-js="show-button"]');
+const showButton = document.querySelector("[data-js=show-button]");
 
 showButton.addEventListener("click", () => {
-  console.log("hi");
+  document.getElementsByClassName("answer")[0].style.color = "black";
 });
 
 // Dark Mode
 
-//const darkMode = document.querySelector('[data-js="dark"]');
+const darkMode = document.querySelector("[data-js=dark]");
 
 //const mainElement = document.querySelector('[data-js="main"]');
 
-//darkMode.addEventListener("click", () => {
-//console.log("hallo");
-//});
+darkMode.addEventListener("click", () => {
+  document.getElementsByClassName("darkmain")[0].style.backgroundColor =
+    "black";
+});
 
 //mainElement.classList.add('dark');//
 //});
-
-//console.log("hallo");
