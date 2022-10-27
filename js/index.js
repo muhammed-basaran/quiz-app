25c0c2707eeebaf4eb2d794cd23ff50bc0f26639
Muhammed Basaran <mbasaran0293@gmail.com>
2022-10-27 09:33:51 +0200

    separate the style sheets, add some text

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const darkMode = document.querySelector('[data-js="dark"]');
+
+const mainElement = document.querySelector('[data-js="main"]');
+
+darkMode.addEventListener("click", () => {
+  console.log("hallo");
+});
+
+//mainElement.classList.add('dark');//
+//});
+
+//console.log("hallo");
